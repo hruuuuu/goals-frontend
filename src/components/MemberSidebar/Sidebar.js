65c87f68bc013290beb7b849681579efff73cba6
nick840290 <nick840290@gmail.com>
2022-02-10 15:36:08 +0800

    <ADD> MemberSidebar (Desktop)

---
+++ src/components/MemberSidebar/Sidebar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Sidebar(props) {
+  const { SidebarLinks } = props;
+  return (
+    <div className="d-none d-lg-flex">
+      <ul className="Sidebaritems">
+        {SidebarLinks.map((link) => {
+          return (
+            <li className="Sidebaritem" key={link.id}>
+              <NavLink to={link.router} className="Sidebarfont">
+                {link.icon} &nbsp;
+                {link.name}
+              </NavLink>
+              {link.line}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
