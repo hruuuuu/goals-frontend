2906f3018c60412b7256c6143bbd62c5a459383c
Allie <huaruhsiao.b@gmail.com>
2022-02-10 12:06:56 +0800

    [UPDATE] [P] Products

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import Header from '../components/Header';
+import Filter from '../components/Products/Filter';
+import ProductList from '../components/Products/ProductList';
 
 function Products() {
   return (
     <>
       <Header />
+      <div className="container">
+        <div className="row gx-5">
+          <div className="col-4">
+            <Filter />
+          </div>
+          <div className="col-8">
+            <ProductList />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
