097e767a5f6d08079a85b16e50e002c3d1f8eb94
Chris9271 <chrisweng71@outlook.com>
2022-02-11 10:48:46 +0800

    [UPDATE] Login & Signup pages css file

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,87 +18,75 @@ const Login = () => {
 
   return (
     <>
-      <div className="form position-relative d-flex">
-        <img src={Image} alt="" className="bgImage position-absolute" />
-        <div className="signup-form bg-light">
-          <div className="container h-100 d-flex flex-column justify-content-center align-items-center">
-            <div className="text-center">
-              <h1 className="title border border-top-0 border-start-0 border-end-0 border-warning border-5 pb-4">
-                登入
-              </h1>
+      <div className="loginWrapper">
+        <img src={Image} alt="" className="loginBackground" />
+        <div className="loginFormWrapper">
+          <h1 className="formTitle">登入</h1>
+          <form className="loginForm">
+            <div className="form-floating mb-3">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="請輸入電子信箱"
+                className="form-control"
+              />
+              <i className="fas fa-envelope"></i>
+              <label htmlFor="email" className="inputLabel">
+                E-mail
+              </label>
             </div>
-            <form className="pt-5">
-              <div className="form-floating mb-3">
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="請輸入電子信箱"
-                  className="form-control"
-                />
-                <i className="fas fa-envelope"></i>
-                <label htmlFor="email" className="text-secondary">
-                  E-mail
-                </label>
-              </div>
-              <div className="form-floating mb-3">
-                <input
-                  type="password"
+            <div className="form-floating mb-3">
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="請輸入密碼"
+                className="form-control"
+              />
+              <i className="fas fa-lock"></i>
+              {showPassword && eye === 'password' ? (
+                <i
+                  className="fas fa-eye-slash"
                   id="password"
-                  name="password"
-                  placeholder="請輸入密碼"
-                  className="form-control"
-                />
-                <i className="fas fa-lock"></i>
-                {showPassword && eye === 'password' ? (
-                  <i
-                    className="fas fa-eye-slash"
-                    id="password"
-                    onClick={(e) => handleSwitchEyes(e)}
-                  ></i>
-                ) : (
-                  <i
-                    className="fas fa-eye"
-                    id="password"
-                    onClick={(e) => handleSwitchEyes(e)}
-                  ></i>
-                )}
-                <label htmlFor="password" className="text-secondary">
-                  Password
-                </label>
-              </div>
-              <div className="d-flex justify-content-around">
-                <p className="text-warning text-center py-3 mb-0">
-                  尚未有帳戶？
-                </p>
-                <p className="text-warning text-center py-3 mb-0">忘記密碼</p>
-              </div>
-              <button className="btn btn-primary w-100 mt-3 py-3">
-                Log In
+                  onClick={(e) => handleSwitchEyes(e)}
+                ></i>
+              ) : (
+                <i
+                  className="fas fa-eye"
+                  id="password"
+                  onClick={(e) => handleSwitchEyes(e)}
+                ></i>
+              )}
+              <label htmlFor="password" className="inputLabel">
+                Password
+              </label>
+            </div>
+            <div className="notificationWrapper">
+              <p className="notification">尚未有帳戶？</p>
+              <p className="notification">忘記密碼</p>
+            </div>
+            <button className="loginBtn">Log In</button>
+          </form>
+          <div className="anotherLoginMethod">
+            <p className="socialTag">
+              &mdash;
+              <span className="socialTitle">Or Continue With Social Media</span>
+              &mdash;
+            </p>
+            <div className="btnGroup">
+              <button className="btn google">
+                <i className="fab fa-google"></i>
               </button>
-            </form>
-            <div className="third-party">
-              <p className="pt-4 mt-4 pb-3">
-                &mdash;
-                <span className="bg-light">
-                  Or continue with social media account
-                </span>
-                &mdash;
-              </p>
-              <div className="d-flex justify-content-between">
-                <button className="btn btn-primary bg-light text-primary px-4 py-3">
-                  <i className="fab fa-google"></i>
-                </button>
-                <button className="btn btn-primary bg-light facebook px-4 py-3">
-                  <i className="fab fa-facebook"></i>
-                </button>
-                <button className="btn btn-primary bg-light line px-4 py-3">
-                  <i className="fab fa-line"></i>
-                </button>
-                <button className="btn btn-primary bg-light apple px-4 py-3">
-                  <i className="fab fa-apple"></i>
-                </button>
-              </div>
+              <button className="btn facebook">
+                <i className="fab fa-facebook"></i>
+              </button>
+              <button className="btn line">
+                <i className="fab fa-line"></i>
+              </button>
+              <button className="btn apple">
+                <i className="fab fa-apple"></i>
+              </button>
             </div>
           </div>
         </div>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -18,89 +18,82 @@ const Signup = () => {
 
   return (
     <>
-      <div className="form position-relative d-flex">
-        <img src={Image} alt="" className="bgImage position-absolute" />
-        <div className="signup-form bg-light">
-          <div className="container h-100 d-flex flex-column justify-content-center align-items-center">
-            <div className="text-center">
-              <h1 className="title border border-top-0 border-start-0 border-end-0 border-warning border-5 pb-4">
-                註冊
-              </h1>
+      <div className="signWrapper">
+        <img src={Image} alt="" className="signBackground" />
+        {/* container for signFormWrapper */}
+        <div className="signFormWrapper">
+          {/* text-center for div below */}
+          <h1 className="formTitle">註冊</h1>
+          <form className="signForm">
+            <div className="form-floating">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="請輸入電子信箱"
+                className="form-control"
+              />
+              <i className="fas fa-envelope"></i>
+              <label htmlFor="email" className="inputLabel">
+                E-mail
+              </label>
             </div>
-            <form className="pt-5">
-              <div className="form-floating mb-3">
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="請輸入電子信箱"
-                  className="form-control"
-                />
-                <i className="fas fa-envelope"></i>
-                <label htmlFor="email" className="text-secondary">
-                  E-mail
-                </label>
-              </div>
-              <div className="form-floating mb-3">
-                <input
-                  type="password"
+            <div className="form-floating">
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="請輸入密碼"
+                className="form-control"
+              />
+              <i className="fas fa-lock"></i>
+              {showPassword && eye === 'password' ? (
+                <i
+                  className="fas fa-eye-slash"
                   id="password"
-                  name="password"
-                  placeholder="請輸入密碼"
-                  className="form-control"
-                />
-                <i className="fas fa-lock"></i>
-                {showPassword && eye === 'password' ? (
-                  <i
-                    className="fas fa-eye-slash"
-                    id="password"
-                    onClick={(e) => handleSwitchEyes(e)}
-                  ></i>
-                ) : (
-                  <i
-                    className="fas fa-eye"
-                    id="password"
-                    onClick={(e) => handleSwitchEyes(e)}
-                  ></i>
-                )}
-                <label htmlFor="password" className="text-secondary">
-                  Password
-                </label>
-              </div>
-              <div className="form-floating mb-3">
-                <input
-                  type="password"
+                  onClick={(e) => handleSwitchEyes(e)}
+                ></i>
+              ) : (
+                <i
+                  className="fas fa-eye"
+                  id="password"
+                  onClick={(e) => handleSwitchEyes(e)}
+                ></i>
+              )}
+              <label htmlFor="password" className="inputLabel">
+                Password
+              </label>
+            </div>
+            <div className="form-floating">
+              <input
+                type="password"
+                id="confirmPassword"
+                name="confirmPassword"
+                placeholder="請再次輸入密碼"
+                className="form-control"
+              />
+              <i className="fas fa-lock"></i>
+              {showPassword && eye === 'confirmPassword' ? (
+                <i
+                  className="fas fa-eye-slash"
                   id="confirmPassword"
-                  name="confirmPassword"
-                  placeholder="請再次輸入密碼"
-                  className="form-control"
-                />
-                <i className="fas fa-lock"></i>
-                {showPassword && eye === 'confirmPassword' ? (
-                  <i
-                    className="fas fa-eye-slash"
-                    id="confirmPassword"
-                    onClick={(e) => handleSwitchEyes(e)}
-                  ></i>
-                ) : (
-                  <i
-                    className="fas fa-eye"
-                    id="confirmPassword"
-                    onClick={(e) => handleSwitchEyes(e)}
-                  ></i>
-                )}
-                <label htmlFor="confirmPassword" className="text-secondary">
-                  Confirm Password
-                </label>
-              </div>
-              <p className="text-warning text-center py-3 mb-0">
-                已經有帳戶了？
-              </p>
-              <button className="btn btn-primary w-100 mt-3 py-3">
-                Sign In
-              </button>
-            </form>
-          </div>
+                  onClick={(e) => handleSwitchEyes(e)}
+                ></i>
+              ) : (
+                <i
+                  className="fas fa-eye"
+                  id="confirmPassword"
+                  onClick={(e) => handleSwitchEyes(e)}
+                ></i>
+              )}
+              <label htmlFor="confirmPassword" className="inputLabel">
+                Confirm Password
+              </label>
+            </div>
+            <p className="notification">已經有帳戶了？</p>
+            {/* w-100 for below */}
+            <button className="signBtn btn">Sign In</button>
+          </form>
         </div>
       </div>
     </>
