607b8d8b61827ccc9e687399b0590c8a3076b20c
Allie <huaruhsiao.b@gmail.com>
2022-02-11 23:02:19 +0800

    [ADD] [C] 1. FloatingIcon & FloatingMenu layout, style & RWD 2. animation scss

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import { React, useState } from 'react';
+
 import Header from '../components/Header';
 import Filter from '../components/Products/Filter';
 import ProductList from '../components/Products/ProductList';
+import FloatingIcon from '../components/FloatingIcon';
+import FloatingMenu from '../components/FloatingMenu';
 
 function Products() {
+  const [isDisplay, setIsDisplay] = useState(false);
   return (
     <>
       <Header />
       <div className="container">
-        <div className="row gx-5">
+        <div className="row gx-lg-5">
           <div className="col-3 d-none d-lg-block">
             <Filter />
           </div>
@@ -16,6 +20,10 @@ function Products() {
             <ProductList />
           </div>
         </div>
+        <FloatingIcon setIsDisplay={setIsDisplay} />
+        {isDisplay && (
+          <FloatingMenu isDisplay={isDisplay} setIsDisplay={setIsDisplay} />
+        )}
       </div>
     </>
   );
