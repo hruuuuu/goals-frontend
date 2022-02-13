5e5aeaf41ba45b24dcedae4a5a60aceb8e5b2b0a
nick840290 <nick840290@gmail.com>
2022-02-13 11:24:15 +0800

    [ADD]floating icon in member page

--- src/pages/Member.js
+++ src/pages/Member.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import { React, useState } from 'react';
 import Header from '../components/Header';
 import MemberSidebar from '../components/MemberSidebar';
 import EditProfile from '../components/Member/EditProfile';
 import EditPassWord from '../components/Member/EditPassword';
+import FloatingIcon from '../components/MemberSidebar/FloatingIcon';
+import FloatingMenu from '../components/FloatingMenu';
 
 function Member() {
+  const [isDisplay, setIsDisplay] = useState(false);
   return (
     <>
       <Header />
@@ -18,6 +21,13 @@ function Member() {
             <EditProfile />
             <EditPassWord />
           </div>
+        </div>
+
+        <div className="d-md-none">
+          <FloatingIcon setIsDisplay={setIsDisplay} />
+          {isDisplay && (
+            <FloatingMenu isDisplay={isDisplay} setIsDisplay={setIsDisplay} />
+          )}
         </div>
       </div>
     </>
