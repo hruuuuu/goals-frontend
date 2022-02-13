d9049c77b0603eeb9e7031ce679e3c91bcb389be
nick840290 <nick840290@gmail.com>
2022-02-13 18:13:09 +0800

    [ADD]

--- src/components/Member/EditPassword.js
+++ src/components/Member/EditPassword.js
@@ -6,37 +6,37 @@ const EditPassWord = () => {
       <div className="alert" style={{ background: '#FAFAFA' }} role="alert">
         修改密碼
       </div>
-      <label htmlFor="OldPassword" className="form-label ">
+      <label htmlFor="OldPassword" className="form-label c-search__heading">
         舊密碼
       </label>
       <div className=" mb-2">
         <input
           type="password"
-          className="form-control"
+          className="form-control password__input c-input"
           id="OldPassword"
           placeholder="請輸入..."
         />
       </div>
 
-      <label htmlFor="NewPassword" className="form-label">
+      <label htmlFor="NewPassword" className="form-label c-search__heading">
         新密碼
       </label>
       <div className=" mb-2">
         <input
           type="password"
-          className="form-control"
+          className="form-control password__input__input c-input"
           id="NewPassword"
           placeholder="請輸入..."
         />
       </div>
 
-      <label htmlFor="ConfirmPassword" className="form-label">
+      <label htmlFor="ConfirmPassword" className="form-label c-search__heading">
         確認密碼
       </label>
       <div className=" mb-2">
         <input
           type="password"
-          className="form-control"
+          className="form-control password__input__input c-input"
           id="ConfirmPassword"
           placeholder="請輸入..."
         />
@@ -45,8 +45,7 @@ const EditPassWord = () => {
       <div>
         <button
           type="submit"
-          className="btn btn-primary col-12"
-          style={{ background: '#6B9C66' }}
+          className="c-product-filter__action e-btn--primary e-btn--medium col-12"
         >
           儲存變更
         </button>
