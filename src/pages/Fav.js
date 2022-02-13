6c16fcf5651fcf87996fa08f1ce5d65a38606723
nick840290 <nick840290@gmail.com>
2022-02-13 16:57:35 +0800

    [ADD]

--- src/pages/Fav.js
+++ src/pages/Fav.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import { React, useState } from 'react';
 import Header from '../components/Header';
 import MemberSidebar from '../components/MemberSidebar';
+import FloatingModal from '../components/MemberSidebar/FloatingModal';
+import FloatingIcon from '../components/MemberSidebar/FloatingIcon';
 
 function Fav() {
+  const [isDisplay, setIsDisplay] = useState(false);
   return (
     <>
       <Header />
@@ -10,10 +13,18 @@ function Fav() {
 
       <div className="container">
         <div className="row">
-          <div className="col-lg-3 d-none d-md-block">
+          <div className="col-md-3 d-none d-md-block">
             <MemberSidebar />
           </div>
-          <div className="col col-lg-9">{/* components可以放這裡 */}</div>
+          <div className="col col-md-9">{/* components可以放這裡 */}</div>
+        </div>
+
+        {/* 浮動視窗 */}
+        <div className="d-md-none">
+          <FloatingIcon setIsDisplay={setIsDisplay} />
+          {isDisplay && (
+            <FloatingModal isDisplay={isDisplay} setIsDisplay={setIsDisplay} />
+          )}
         </div>
       </div>
     </>
