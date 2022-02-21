8b8593df36d8c254ed5b85e17b69b85965d98085
Allie <huaruhsiao.b@gmail.com>
2022-02-21 22:50:04 +0800

    [UPDATE] [C] FloatingChatModal layout & style

--- src/components/FloatingChatModal.js
+++ src/components/FloatingChatModal.js
@@ -30,13 +30,23 @@ function FloatingChatModal(props) {
 
   const sendMessage = () => {
     // emit: 指定頻道 傳送訊息給server
-    ws.emit(roomName, { sender: 'user', message: sendingMessage });
-    setSendingMessage('');
+    if (sendingMessage !== '') {
+      ws.emit(roomName, { sender: 'user', message: sendingMessage });
+      setSendingMessage('');
+    }
   };
 
   const handleFieldChange = (e) => {
-    setSendingMessage(e.target.value);
+    if (e.target.value !== '') {
+      setSendingMessage(e.target.value);
+    }
   };
+
+  // const handleKeyDown = (e) => {
+  //   if (e.key === 'Enter') {
+  //     sendMessage();
+  //   }
+  // };
 
   /* 如果有連線到 就把webSocket寫到ws state裡
   是用來配合useEffect+相依性陣列檢查是否有連線成功 */
@@ -63,47 +73,68 @@ function FloatingChatModal(props) {
         }`}
       >
         <div className={`c-floating-menu__content`}>
-          {!ws && (
-            <button type="button" onClick={connectWebSocket}>
-              聯繫客服
-            </button>
-          )}
-          {!isEmptyMessage &&
-            messages.map((msg, index) => {
-              const { sender, message } = msg;
-              return (
-                <>
-                  <div key={index} sender={sender}>
-                    <div>{message}</div>
+          <div className="c-chat c-chat--user">
+            <div className="c-chat__header">
+              <div className="c-chat__title">客服</div>
+              <div className="c-chat__subtitle">目前在線</div>
+            </div>
+            <div className="c-chat__content">
+              {!ws && (
+                <button
+                  type="button"
+                  className="e-btn e-btn--primary c-chat__action"
+                  onClick={connectWebSocket}
+                >
+                  聯繫客服
+                </button>
+              )}
+              {!isEmptyMessage &&
+                messages.map((msg, index) => {
+                  const { sender, message } = msg;
+                  return (
+                    <>
+                      <div key={index} className="c-chat__msg" sender={sender}>
+                        <div>{message}</div>
+                      </div>
+                    </>
+                  );
+                })}
+            </div>
+            {ws && (
+              <>
+                <div className="c-chat__footer">
+                  <div className="w-100 d-flex align-items-center justify-content-between">
+                    <input
+                      type="text"
+                      className="form-control c-form__input"
+                      value={sendingMessage}
+                      onChange={handleFieldChange}
+                      // onKeyDown={handleKeyDown}
+                    />
+                    <button
+                      type="button"
+                      className="e-btn e-btn--icon c-chat__btn"
+                      onClick={sendMessage}
+                    >
+                      <i className="fas fa-paper-plane c-chat__icon"></i>
+                    </button>
                   </div>
-                </>
-              );
-            })}
-          {ws && (
-            <>
-              <input
-                type="text"
-                value={sendingMessage}
-                onChange={handleFieldChange}
-              />
-              <button
-                type="button"
-                onClick={() => {
-                  sendMessage();
-                }}
-              >
-                傳送
-              </button>
-            </>
-          )}
+                </div>
+              </>
+            )}
+          </div>
         </div>
         <div
           className="c-floating-menu__bg"
           onClick={() => {
-            setAnimation(true);
-            setTimeout(() => {
-              setIsExpand(false);
-            }, 1000);
+            if (window.confirm('關閉視窗將失去連線 是否確定要退出?')) {
+              setAnimation(true);
+              setTimeout(() => {
+                setIsExpand(false);
+              }, 1000);
+            } else {
+              return;
+            }
           }}
         ></div>
       </div>
