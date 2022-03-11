38104c78ed0803f134268ea88ae857bd2f1958b5
Allie <huaruhsiao.b@gmail.com>
2022-03-11 22:44:43 +0800

    [UPDATE] config remove localhost for testing

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,6 @@
 export const API_URL =
-  (process.env.BACKEND_API_URL || 'http://localhost:3002') + '/api';
+  // (process.env.BACKEND_API_URL || 'http://localhost:3002') + '/api';
+  process.env.BACKEND_API_URL + '/api';
 export const IMG_URL =
   (process.env.BACKEND_API_URL || 'http://localhost:3002') + '/public/img';
 export const BE_URL = process.env.BACKEND_API_URL || 'http://localhost:3002';
